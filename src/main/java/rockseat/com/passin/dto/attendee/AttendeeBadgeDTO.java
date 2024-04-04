c90c0360655e02190181bdf83a8a3b18b83bd801
Andrew <andrewairamdasilva@gmail.com>
2024-04-04 17:07:23 -0300

    feat: attendee dto's

---
+++ src/main/java/rockseat/com/passin/dto/attendee/AttendeeBadgeDTO.java
@@ -0,0 +1,4 @@
+package rockseat.com.passin.dto.attendee;
+
+public record AttendeeBadgeDTO(String name, String email, String checkInUrl, String eventId) {
+}
---
+++ src/main/java/rockseat/com/passin/dto/attendee/AttendeeListResponseDTO.java
@@ -0,0 +1,7 @@
+package rockseat.com.passin.dto.attendee;
+
+import java.util.List;
+
+public record AttendeeListResponseDTO(List<AttendeeDetailsDTO> attendees) {
+
+}
